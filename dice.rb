c01e231c7b6c06c653ec8fb314cad9338c133a6e
takumi-aki <takumi.aki@gmail.com>
2012-06-24 16:56:09 +0900

    Mod dice roll method available some dice roll

--- dice.rb
+++ dice.rb
@@ -1,11 +1,21 @@
 class Dice
-  attr_accessor :surface
+  MINIMUM_VALUE = 1
+  DEFAUL_NUMBER_OF_SIDE = 6
+  attr_accessor :number_of_side
 
-  def initialize(surface=6)
-    @surface = surface
+  def initialize(number_of_side=DEFAUL_NUMBER_OF_SIDE)
+    @number_of_side = number_of_side
   end
 
-  def roll
-    rand(surface) + 1
+  def roll(number_of_roll=1)
+    result_numbers = []
+    number_of_roll.times {result_numbers << rand(number_of_side) + 1 }
+
+    result_sum = 0
+    for result in result_numbers
+      result_sum = result_sum + result
+    end
+
+    result_sum
   end
 end
--- test_dice.rb
+++ test_dice.rb
@@ -4,15 +4,32 @@ require_relative 'dice'
 
 class TC_Dice < Test::Unit::TestCase
   def setup
+    @number_of_roll = rand(6) + 1
+    @minimum_total_value = Dice::MINIMUM_VALUE * @number_of_roll
   end
 
-  def test_roll
-    dice = Dice.new(6)
-    set  = Set.new(1..6)
-    assert_block do
-      100.times do
-        set.member?(dice.roll)
-      end
+  def test_roll_one_dice_of_six_side
+    100.times do
+      result = Dice.new.roll
+      assert(result >= Dice::MINIMUM_VALUE)
+      assert(result <= Dice::DEFAUL_NUMBER_OF_SIDE)
+    end
+  end
+
+  def test_roll_some_dice_of_six_sided
+    100.times do
+      result = Dice.new.roll(@number_of_roll)
+      assert(result >= @minimum_total_value)
+      assert(result <= Dice::DEFAUL_NUMBER_OF_SIDE * @number_of_roll)
+    end
+  end
+
+  def test_roll_some_dice_of_some_side
+    100.times do
+      number_of_side = rand(10) + 1
+      result = Dice.new(number_of_side).roll(@number_of_roll) 
+      assert(result >= @minimum_total_value)
+      assert(result <= number_of_side * @number_of_roll)
     end
   end
 end
