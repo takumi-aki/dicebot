7db5979775b3b2467cf3f08a89165b294dd44120
takumi-aki <takumi.aki@gmail.com>
2012-06-24 12:59:50 +0900

    Rename dice_test.rb

---
+++ test_dice.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require 'set'
+require_relative 'dice'
+
+class TC_Dice < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_roll
+    dice = Dice.new(6)
+    set  = Set.new(1..6)
+    assert_block 100.times do
+      set.member?(dice.roll)
+    end
+  end
+end
